f9447c68613bd83838b094096dee5abdafa85e66
Ammar Syed <asyed@ufl.edu>
2020-07-19 22:45:53 -0400

    Added Fullname
    Basically updated professorController, studentController, studentModel, professorModel in the server folder

--- CEN3031Project/server/controllers/professorController.js
+++ CEN3031Project/server/controllers/professorController.js
@@ -1,29 +1,38 @@
 import Professor from '../models/professorModel.js';
 
-export const create = async (req, res) => {
+export const create = async (req, res) =>
+{
 
     var professor = new Professor(req.body);
+    professor.fullName = professor.firstName + " " + professor.lastName;
 
-    professor.save(function (err, result) {
-        if (err) {
+    professor.save(function (err, result)
+    {
+        if(err)
+        {
             res.send(err);
         }
-        else {
-            res.send({ 'success': true, 'message': 'Professor retrieved for save', result });
+        else
+        {
+            res.send({'success': true, 'message': 'Professor retrieved for save', result});
         }
     });
 };
 
-export const checkLogin = (req, res) => {
+export const checkLogin = (req, res) =>
+{
 
     const blank = {};
     var query = Professor.find(blank);
 
-    query.exec(function (err, all) {
-        if (err) {
+    query.exec(function (err, all)
+    {
+        if(err)
+        {
             res.status(404).send(err);
         }
-        else {
+        else
+        {
             res.json(all);
         }
     });
--- CEN3031Project/server/controllers/studentController.js
+++ CEN3031Project/server/controllers/studentController.js
@@ -1,29 +1,39 @@
 import Student from '../models/studentModel.js';
 
-export const create = async (req, res) => {
+export const create = async (req, res) =>
+{
 
     var student = new Student(req.body);
-    
-    student.save(function (err, result) {
-        if (err) {
+
+    student.fullName = student.firstName + " " + student.lastName;
+
+    student.save(function (err, result)
+    {
+        if(err)
+        {
             res.send(err);
         }
-        else {
-            res.send({ 'success': true, 'message': 'Student retrieved for save', result });
+        else
+        {
+            res.send({'success': true, 'message': 'Student retrieved for save', result});
         }
     });
 };
 
-export const checkLogin = (req, res) => {
+export const checkLogin = (req, res) =>
+{
 
     const blank = {};
     var query = Student.find(blank);
 
-    query.exec(function (err, all) {
-        if (err) {
+    query.exec(function (err, all)
+    {
+        if(err)
+        {
             res.status(404).send(err);
         }
-        else {
+        else
+        {
             res.json(all);
         }
     });
